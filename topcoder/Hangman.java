a73b78fe8c1e1fcf5271e0f20dc071cc1403da39
humblesticker <humblesticker@gmail.com>
2016-03-24 17:42:08 -0700

    SRM-190 refactor

--- topcoder/Hangman.java
+++ topcoder/Hangman.java
@@ -1,31 +1,21 @@
-/*
-	for each word in words
-		compare with feedback 
-			if - skp
-			else match
-		if matched update matched and count++
-		if count > 1 return ""
-	if count == 0 return matched
-	else reutrn ""
-*/
+import java.util.*;
+
 public class Hangman {
-	private boolean matched(String feedback, String word) {
-		if(feedback.length() != word.length()) return false;
-		for(int i=0; i<feedback.length(); i++) {
-			char f = feedback.charAt(i), w = word.charAt(i);
-			if(f == '-') { if(feedback.indexOf(w) >= 0) return false; }
-			else if(f != word.charAt(i)) return false;
+	private boolean matching(String feedback, String word) {
+		if(feedback.length() != word.length()) return false; 
+		for(int i=0; i<word.length(); i++) {
+			char w = word.charAt(i), f = feedback.charAt(i);
+			if(feedback.indexOf(w) >= 0) { if(w != f) return false; } 
+			else { if(f != '-') return false; }
 		}
 		return true;
 	}
-	
+
 	public String guessWord(String feedback, String[] words) {
-		int count = 0; String matched = "";
-		for(String word : words) {
-			if(matched(feedback, word)) { matched = word; count++; }
-			if(count > 1) return "";
-		}
-		if(count == 1) return matched;
-		else return "";
+		List<String> list = new ArrayList<String>();
+		for(String word : words) if(matching(feedback, word)) list.add(word);
+
+		if(list.size() == 1) return list.get(0);
+		return "";
 	}
 }
