646a3ff52c2edbbb16cb77031026303e63d71b8f
humblesticker <humblesticker@gmail.com>
2016-07-01 23:08:10 -0700

    SRM-216

--- topcoder/RockStar.java
+++ topcoder/RockStar.java
@@ -1,13 +1,12 @@
 public class RockStar {
 	private int count(int a, int b, int c, int d) {
 		if(b == 0) return a;
-		if(b>c) return a + 2*c + 1 + d;
+		if(b > c) return a + 2*c + 1 + d;
 		return a + 2*b + d;
 	}
 
 	public int getNumSongs(int ff, int fs, int sf, int ss) {
-		int fast = count(ff, fs, sf, ss), slow = count(ss, sf, fs, ff);
-		if(ff > 0 || fs > 0) return fast;
-		return Math.max(fast, slow);
+		int fast = count(ff, fs, sf, ss); if(ff > 0 || fs > 0) return fast;
+		return Math.max(fast, count(ss, sf, fs, ff));
  	}
 }
