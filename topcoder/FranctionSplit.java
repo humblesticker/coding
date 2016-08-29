85955c17d07621c774b839e6731a6619fbc3180f
humblesticker <humblesticker@gmail.com>
2016-08-28 18:46:46 -0700

    topcoder > FractionSplit refactor

--- topcoder/FranctionSplit.java
+++ topcoder/FranctionSplit.java
@@ -1,57 +1,14 @@
-/*
-1. luf 
-	from i=2 to forever 
-		if 1/i < n/d return i
-
-2. update n/d
-	gcd = gcd(i, d)
-	d: i * d / gcd
-	n: (n * i - d) / gcd
-	
-List<String> out
-while n > 0
-	i = luf(n, d) 
-	add 1/i to out
-	update n, d 
-	
-Analysis ?
-*/
 import java.util.*;
 
 public class FractionSplit {
-	int getLuf(int n, int d) {
-		double f = (double)n/d;
-		int i=2; 
-		while(true) {
-			if(1.0/i <= f) return i;
-			i++;
-		}
-	}
-	
-	int getGcd(int s, int l) {
-		while(s != 0) {
-			int r = l%s;
-			l = s; s = r;
-		}
-		return l;
-	}
-	
 	public String[] getSum(int n, int d) {
 		List<String> out = new ArrayList<String>();
 		while(n > 0) {
-			int luf = getLuf(n, d);
-			out.add("1/" + luf);
-			
-			int gcd = getGcd(luf, d);
-			n = (n * luf - d) / gcd; d = luf * d / gcd;
+			double f = (double)n/d;
+			int i=2; while(1.0/i > f) i++;
+			out.add("1/" + i);
+			n = n * i - d; d *= i;
 		}
 		return out.toArray(new String[0]);
 	}
 }
-		
-		
-	
-	
-	
-	
-	 
