aa7cf76bd5b3d2864b23ffe92ba8a3655e534e90
humblesticker <humblesticker@gmail.com>
2016-08-31 23:32:47 -0700

    SRM-235 > StepperMotor refactor

--- topcoder/StepperMotor.java
+++ topcoder/StepperMotor.java
@@ -1,22 +1,28 @@
 public class StepperMotor {
-	long getmin(long c, long t, long n) {
-		if(c == t) return 0;
-		
-		long pos = (t - c)%n, neg = pos - n; 
-		if(c > t) { neg = (t - c)%n; pos = n + neg; }
-	
-		if(Math.abs(neg) < pos) return neg;
-		return pos;
+	// tricky
+	int less(int a, int b) {
+		int aa = Math.abs(a), ab = Math.abs(b);
+		if(aa == ab) return Math.max(a, b);
+		return aa < ab ? a : b;
+	}
+
+	// rotation
+	int getmin(int c, int t, int n) {
+		int d = t - c; if(d < 0) d += n;
+		return less(d, d-n);
 	}
 	
+	// loop
 	public int rotateToNearest(int n, int c, int[] target) {
-		c %= n;
+		c %= n; if(c < 0) c += n;
 		int min = Integer.MAX_VALUE;
+
 		for(int t : target) {
-			t %= n;
-			int local = (int)getmin(c, t, n), amin = Math.abs(min), alocal = Math.abs(local);
-			if(alocal < amin || (alocal == amin && local > 0)) min = local;
+			t %= n; if(t < 0) t += n;
+			if(c == t) return 0;
+			min = less(min, getmin(c, t, n));
 		}
+
 		return min; 
 	}
 }
