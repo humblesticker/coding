fe117f805a41a36d8aa2f74f60201e1b27b1adcb
Brian Choi <bchoi@coreipsolutions.com>
2016-09-08 11:34:30 -0500

    SRM > GuessCard: using 2d array and subroutines

--- topcoder/GuessCard.java
+++ topcoder/GuessCard.java
@@ -1,33 +1,40 @@
 import java.util.*;
+
 public class GuessCard {
-	public int whichRow(int w, int h, int[] columns) {
+	int[][] convert(int[][] cards) {
+		int count = 0, w = cards[0].length, h = cards.length;
+		int[][] converted = new int[h][w]; 
+		for(int j=0; j<w; j++) for(int i=0; i<h; i++) { converted[count/w][count%w] = cards[i][j]; count++; }
+		return converted;
+	}
+
+	int[][] init(int w, int h) {
+		int[][] cards = new int[h][w]; int count = 0;
+		for(int i=0; i<h; i++) for(int j=0; j<w; j++) cards[i][j] = ++count;
+		return cards;
+	}
+
+	Set<Integer> column(int[][] cards, int col) {
+	    int w = cards[0].length, h = cards.length;
 		Set<Integer> set = new HashSet<Integer>();
-		int[] cards = new int[w*h];
-		for(int i=0; i<cards.length; i++) { 
-			cards[i] = i+1; if(i%w == columns[0]) set.add(i+1); 
-		}
+		for(int i=0; i<h; i++) for(int j=0; j<w; j++) if(j==col) set.add(cards[i][j]);
+		return set;
+	}
+
+	public int whichRow(int w, int h, int[] columns) {
+		int[][] cards = init(w, h);
+		Set<Integer> set = column(cards, columns[0]);
 		
 		for(int i=1; i<columns.length; i++) {
-			Set<Integer> set1 = new HashSet<Integer>();
-			int[] cards1 = new int[w*h]; int index = 0;
-
-			for(int col=0; col<w; col++) 
-				for(int row=col; row<cards1.length; row+=w) { 
-					cards1[index] = cards[row]; 
-					if(index%w == columns[i]) set1.add(cards1[index]);
-					index++;
-		 		}
-		 		
-		 	set.retainAll(set1);
-		 	cards = cards1;
+			cards = convert(cards);
+			Set<Integer> local = column(cards, columns[i]);
+		 	set.retainAll(local);
 		}
 		
 		if(set.size() == 1) {
-			int v = set.iterator().next(), i=0;
-			for(i=0; i<cards.length; i++) if(cards[i] == v) break;
-			return i/w;
+			int v = set.iterator().next();
+			for(int i=0; i<h; i++) for(int j=0; j<w; j++) if(cards[i][j] == v) return i;
 		}
 		return -1;
 	}
-}
-		
+}		
