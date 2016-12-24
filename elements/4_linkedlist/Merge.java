5cc8b36d08bf9a00886507bb2e9874417de81e9c
humblesticker <humblesticker@gmail.com>
2016-12-24 11:12:52 -0800

    elements > linked list merge, simpler

--- elements/4_linkedlist/Merge.java
+++ elements/4_linkedlist/Merge.java
@@ -1,22 +1,15 @@
 /*
-assume list is sorted
-case
-	empty l1, l2
-	l1 larger, l2 larger, l1 and l2 same size
+linked list is a pointer, merge into new list --> simpler
 */
 public class Merge {
 	public static void merge(LinkedList<Integer> list1, LinkedList<Integer> list2) {
-		Node<Integer> n1 = list1.head.next, n2 = list2.head.next, prev = list1.head;
+		Node<Integer> n1 = list1.head.next, n2 = list2.head.next, cur = list1.head;
 		while(n1 != null && n2 != null) {
-			if(n1.data < n2.data) { prev = n1; n1 = n1.next; }
-			else { 
-				Node<Integer> insert = n2; n2 = n2.next; // save n2
-				prev.next = insert; insert.next = n1; // insert before n1
-				prev = insert;
-			}
+			if(n1.data < n2.data) { cur.next = n1; n1 = n1.next; }
+			else { cur.next = n2; n2 = n2.next; }
+			cur = cur.next;
 		}
-
-		if(n2 != null) prev.next = n2; // connect rest of n2 to the end of n1
+		cur.next = (n1 != null ? n1 : n2);
 	}
 
 	public static void main(String[] args) {
@@ -30,7 +23,7 @@ public class Merge {
 		list2.add(2);
 		list2.print();
 
-		merge(list2, list1);
-		list2.print();
+		merge(list1, list2);
+		list1.print();
 	}
 }
