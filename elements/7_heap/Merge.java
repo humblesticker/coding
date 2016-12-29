49c28da210b80d6029ad9d668a0f0b0919ac75b8
humblesticker <humblesticker@gmail.com>
2016-12-28 23:19:13 -0800

    elements > heap

--- elements/7_heap/Merge.java
+++ elements/7_heap/Merge.java
@@ -1,10 +1,42 @@
-/*
-
-*/
 import java.util.*;
 
 public class Merge {
-	public List<Integer> merge(List<List<Integer> sortedArrays) {
-
+	class Item {
+		int id; int value; Item(int i, int v) { id = i; value = v; }
 	}
+
+	public List<Integer> merge(List<List<Integer>> sortedArrays) {
+		List<Iterator<Integer>> iters = new ArrayList<>();
+		for(List<Integer> list : sortedArrays) iters.add(list.iterator());	
+
+		List<Integer> sorted = new LinkedList<>();
+		PriorityQueue<Item> pq = new PriorityQueue<>((i1, i2) -> i1.value - i2.value); 
+
+		int i = 0;
+		for(Iterator<Integer> iter : iters) 
+			if(iter.hasNext()) pq.offer(new Item(i++, iter.next())); 
+
+		while(pq.size() > 0) {
+			Item min = pq.poll(); sorted.add(min.value);
+			Iterator<Integer> iter = iters.get(min.id); 
+			if(iter.hasNext()) pq.offer(new Item(min.id, iter.next()));
+		}
+
+		return sorted; 
+	}
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int k = s.nextInt();
+		List<List<Integer>> arrays = new LinkedList<>();
+
+		while(k-- > 0) {
+			int n = s.nextInt(); 
+			List<Integer> list = new LinkedList<>();
+			while(n-- > 0) list.add(s.nextInt());
+			arrays.add(list);
+		}
+
+		System.out.println((new Merge()).merge(arrays));
+	}	
 }
