b44295398f2993d52a2a93ffd4fd8c51c21ad789
humblesticker <humblesticker@gmail.com>
2016-12-31 10:44:51 -0800

    leetcode > merge list with sentinel

--- leetcode/LinkedList.java
+++ leetcode/LinkedList.java
@@ -1,5 +1,6 @@
 /*
-
+TODO: recursive ?
+iterative with sentinel
 */
 public class LinkedList {
     class ListNode {
@@ -9,20 +10,14 @@ public class LinkedList {
     }
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1 == null && l2 != null) return l2;
-        if(l2 == null && l1 != null) return l1;
-        
-        ListNode head = null, prev = null;
+        ListNode head = new ListNode(-1), prev = head; 
         while(l1 != null && l2 != null) {
-            int val = 0;
-            if(l1.val > l2.val) { val = l2.val; l2 = l2.next; }
-            else { val = l1.val; l1 = l1.next; }
-            
-            ListNode node = new ListNode(val);
-            if(head == null) head = node; else prev.next = node; 
-            prev = node;
+            if(l1.val < l2.val) { prev.next = l1; l1 = l1.next; }
+            else { prev.next = l2; l2 = l2.next; }
+            prev = prev.next;
         }
-        if(prev != null) prev.next = (l1 != null ? l1 : l2);
-        return head;
+
+        prev.next = (l1 != null ? l1 : l2);
+        return head.next;
     }
 }
